54596850da2cb9a69b7a96d22ac4abced7af945b
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2021-12-09 14:53:15 +0200

    add validation

--- models/Products.js
+++ models/Products.js
@@ -3,32 +3,32 @@ const { Schema, model } = require('mongoose')
 const schema = new Schema({
     name: {
         type: String,
-        required: true,
+        required: [true,'Name is required'],
         minLength: [6, 'The title must be at least 6 symbols long'],
     },
     keyword: {
         type: String,
-        required: true,
+        required: [true,'Keyword is required'],
         minLength: [6, 'The keyword must be at least 6 symbols long']
     },
     city: {
         type: String,
-        required: true,
+        required: [true,'City is required'],
         maxLength: [10, 'Location should be a maximum of 10 characters long']
     },
     date: {
         type: String,
-        required: true,
+        required: [true,'Date is required'],
         match: [/[0-9]{2}.[0-9]{2}.[0-9]{4}/, 'The Date should be exactly 10 characters - 02.02.2021']
     },
     imageUrl: {
         type: String,
-        required: true,
+        required: [true,'Image is required'],
         match: [/^https?:\/\//, 'The Wildlife Image should start with http:// or https://']
     },
     description: {
         type: String,
-        required: true,
+        required: [true,'Description is required'],
         minLength: [8, 'The Description should be a minimum of 8 characters long.']
     },
     author: { type: Schema.Types.ObjectId, ref: 'User' },
