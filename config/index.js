845fdecb02b268c6a1f4f5867aba9e5b1fa66390
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2022-02-05 17:52:48 +0200

    add procfile, start script,engines,env

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    PORT: 3000,
-    DB_CONNECTION_STRING: 'mongodb://localhost:27017/WildlifePhotography',
+    PORT: process.env.PORT || 3000,
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/WildlifePhotography',
     TOKEN_SECRET:'123456789',
     COOKIE_NAME:'SESSION_DATA',
     
